db4ccfd5a227782a89a26f44e78ce83acdd902f0
Timothy Olanrewaju <TimothyOlanrewaju750@gmail.com>
2024-09-07 14:36:43 +0100

    first commit

--- frontend/src/components/orders/PlaceOrder.jsx
+++ frontend/src/components/orders/PlaceOrder.jsx
@@ -18,7 +18,7 @@ export default function PlaceOrders({ baseURL }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/data/abuja-nasarawa.json");
+        const response = await fetch("/data/nigeria-state-and-lgas.json");
         const data = await response.json();
         setStates(data.states);
       } catch (error) {
@@ -83,115 +83,114 @@ export default function PlaceOrders({ baseURL }) {
       <h3 className="text-center" style={{color:"green"}}>{msg}</h3>
       <h2 className="text-center">Order Form</h2>
       <div className="container">
-      <div className="container col-lg-12 mt-4 d-flex gap-3 justify-content-center align-items-center">
-        <div className="row col-12">
-        <div className="col-lg-6 col-md-6 col-sm-12">
-          <form onSubmit={handleAddItem}>
-            <div className="flex-1 d-flex gap-2 mb-3 h-10">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Add item"
-                value={item}
-                onChange={(e) => setItem(e.target.value)}
-                style={{ height: "60px" }}
-              />
-              <button
-                className="btn btn-primary"
-                onClick={handleAddItem}
-                style={{ height: "60px" }}
-              >
-                Add
-              </button>
-            </div>
-          </form>
-
-          <div className="container">
-            <div className="flex-1 border rounded p-3">
-              {itemList.length === 0 && (
-                <p className="text-center">Start adding items...</p>
-              )}
-              <form onSubmit={handleSubmitOrder}>
-                <ul className="list-group">
-                  {itemList.map((item, index) => (
-                    <li
-                      key={index}
-                      className="list-group-item d-flex justify-content-between align-items-center"
-                    >
-                      {item}
-                      <button
-                        type="button"
-                        className="btn btn-danger btn-sm"
-                        onClick={() => handleDeleteItem(index)}
-                      >
-                        Delete
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-                <br />
-                {/* {itemList.length > 0 && (
-                  <button type="submit" className="btn btn-success w-100">
-                    ORDER
+        <form onSubmit={handleSubmitOrder}>
+          <div className="container col-lg-12 mt-4 d-flex gap-3 justify-content-center align-items-center">
+            <div className="row col-12">
+              <div className="col-lg-6 col-md-6 col-sm-12">
+                <div className="flex-1 d-flex gap-2 mb-3 h-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Add item"
+                    value={item}
+                    onChange={(e) => setItem(e.target.value)}
+                    style={{ height: "60px" }}
+                  />
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={handleAddItem}
+                    style={{ height: "60px" }}
+                  >
+                    Add
                   </button>
-                )} */}
-              </form>
+                </div>
+
+                <div className="container">
+                  <div className="flex-1 border rounded p-3">
+                    {itemList.length === 0 && (
+                      <p className="text-center">Start adding items...</p>
+                    )}
+                    <ul className="list-group">
+                      {itemList.map((item, index) => (
+                        <li
+                          key={index}
+                          className="list-group-item d-flex justify-content-between align-items-center"
+                        >
+                          {item}
+                          <button
+                            type="button"
+                            className="btn btn-danger btn-sm"
+                            onClick={() => handleDeleteItem(index)}
+                          >
+                            Delete
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                    <br />
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12">
+                <div className="form-row mb-2 ">
+                  <div className="col">
+                    <label>State:</label>
+                    <select
+                      id="state"
+                      className="form-control"
+                      value={selectedState}
+                      onChange={handleStateChange}
+                      required
+                    >
+                      <option value="" disabled>Select state</option>
+                      {states.map((state) => (
+                        <option key={state.alias} value={state.alias}>
+                          {state.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="col">
+                    <label>LGA:</label>
+                    <select
+                      id="lga"
+                      className="form-control"
+                      disabled={!selectedState}
+                      onChange={handleLga}
+                      required
+                    >
+                      <option value="">
+                        {!selectedState ? `Select LGA` : `Select LGA in ${selectedState}`}
+                      </option>
+                      {lgas.map((lga, index) => (
+                        <option key={index} value={lga}>
+                          {lga}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+                <div className="form-row mb-3 ">
+                  <div className="col">
+                    <label>Address:</label>
+                    <input
+                      type="text"
+                      id="address"
+                      className="form-control"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-12">
-        <div className="form-row mb-2 ">
-            <div className="col">
-              <label>State:</label>
-              <select
-                id="state"
-                className="form-control"
-                value={selectedState}
-                onChange={handleStateChange} required
-              >
-                <option value="" disabled>Select state</option>
-                {states.map((state) => (
-                  <option key={state.alias} value={state.alias}>
-                    {state.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="col">
-              <label>LGA:</label>
-              <select
-                id="lga"
-                className="form-control"
-                disabled={!selectedState}
-                onChange={handleLga} required
-              >
-                <option value="">{!selectedState ? `Select LGA` : `Select LGA in ${selectedState}`}</option>
-                {lgas.map((lga, index) => (
-                  <option key={index} value={lga}>
-                    {lga}
-                  </option>
-                ))}
-              </select>
-            </div>
-          </div>
-          <div className="form-row mb-3 ">
-            <div className="col">
-              <label>Address:</label>
-              <input
-                type="text"
-                id="address"
-                className="form-control"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)} required
-                
-              />
-            </div>
-          </div>
-        </div>
-        </div>
+          <button type="submit" className="btn btn-success w-100 mt-1">ORDER</button>
+        </form>
       </div>
-        <button type="submit" className="btn btn-success w-100 mt-1">ORDER</button>
-        </div>
+
       <br />
       <br />
 
@@ -200,26 +199,22 @@ export default function PlaceOrders({ baseURL }) {
             <div class="col-md-3 mb-4">
                 <div class="column-header bg-primary text-white text-center">Doctors</div>
                 <div class="column-item">🟢 Doctor 1</div>
-                <div class="column-item">🟡 Doctor 2</div>
-                <div class="column-item">Doctor 3</div>
+                <div class="column-item">🟢 Doctor 2</div>
             </div>
             <div class="col-md-3 mb-4">
                 <div class="column-header bg-primary text-white text-center">Pharmacists</div>
                 <div class="column-item">🟢 Pharmacist 1</div>
-                <div class="column-item">🟡 Pharmacist 2</div>
-                <div class="column-item">Pharmacist 3</div>
+                <div class="column-item">🟢 Pharmacist 2</div>
             </div>
             <div class="col-md-3 mb-4">
                 <div class="column-header bg-primary text-white text-center">Medical Lab Scientists</div>
                 <div class="column-item">🟢 MLS 1</div>
-                <div class="column-item">🟡 MLS 2</div>
-                <div class="column-item">MLS 3</div>
+                <div class="column-item">🟢 MLS 2</div>
             </div>
             <div class="col-md-3 mb-4">
                 <div class="column-header bg-primary text-white text-center">Nurses</div>
                 <div class="column-item">🟢 Nurse 1</div>
-                <div class="column-item">🟡 Nurse 2</div>
-                <div class="column-item">Nurse 3</div>
+                <div class="column-item">🟢 Nurse 2</div>
             </div>
         </div>
     </div>
